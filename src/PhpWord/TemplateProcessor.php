5959e01c3e3dadb78ce6b91649ef206c4c1e0a27
jmangeles99 <jmangeleswork9@gmail.com>
2023-01-09 14:06:02 -0500

    new cloneBlock function

--- src/PhpWord/TemplateProcessor.php
+++ src/PhpWord/TemplateProcessor.php
@@ -871,34 +871,56 @@ class TemplateProcessor
      */
     public function cloneBlock($blockname, $clones = 1, $replace = true, $indexVariables = false, $variableReplacements = null)
     {
-        $xmlBlock = null;
-        $matches = [];
-        preg_match(
-            '/(.*((?s)<w:p\b(?:(?!<w:p\b).)*?\${' . $blockname . '}<\/w:.*?p>))(.*)((?s)<w:p\b(?:(?!<w:p\b).)[^$]*?\${\/' . $blockname . '}<\/w:.*?p>)/is',
-            $this->tempDocumentMainPart,
-            $matches
-        );
+        // Find string position
+        $idx_tag = mb_strpos($this->tempDocumentMainPart, '${'.$blockname.'}');
 
-        if (isset($matches[3])) {
-            $xmlBlock = $matches[3];
-            if ($indexVariables) {
-                $cloned = $this->indexClonedVariables($clones, $xmlBlock);
-            } elseif ($variableReplacements !== null && is_array($variableReplacements)) {
-                $cloned = $this->replaceClonedVariables($variableReplacements, $xmlBlock);
-            } else {
-                $cloned = [];
-                for ($i = 1; $i <= $clones; ++$i) {
-                    $cloned[] = $xmlBlock;
-                }
-            }
+        if ( $idx_tag === false )
+        return null;
 
-            if ($replace) {
-                $this->tempDocumentMainPart = str_replace(
-                    $matches[2] . $matches[3] . $matches[4],
-                    implode('', $cloned),
-                    $this->tempDocumentMainPart
-                );
-            }
+        $idx_start = mb_strrpos(mb_substr($this->tempDocumentMainPart, 0, $idx_tag), '<w:p ');
+        $idx_end   =  mb_strpos($this->tempDocumentMainPart, '${/'.$blockname.'}', $idx_tag);
+
+        if ( $idx_start === false || $idx_end === false )
+        return null;
+
+        $idx_end = mb_strpos($this->tempDocumentMainPart, '</w:p>', $idx_end);
+
+        if ( $idx_end === false )
+        return null;
+
+        $idx_end += 6;
+
+        $xml = mb_substr($this->tempDocumentMainPart, $idx_start, $idx_end - $idx_start);
+
+        // --- //
+
+        $idx_content_start =   mb_strpos($xml, 'p>');
+        $idx_content_end   =  mb_strrpos($xml, '<w:p ');
+
+        if ( $idx_content_start === false || $idx_content_end === false )
+        return null;
+
+        $idx_content_start += 2;
+
+        $xmlBlock = mb_substr($xml, $idx_content_start, $idx_content_end - $idx_content_start);
+
+        // Replace block values //
+
+        if ( $replace )
+        {
+            $arrValue = array();
+
+            if ( $indexVariables )
+                $arrValue = $this->indexClonedVariables($clones, $xmlBlock);
+            elseif ( $variableReplacements !== null && is_array($variableReplacements) )
+                $arrValue = $this->replaceClonedVariables($variableReplacements, $xmlBlock);
+            else
+                for ( $i = 1 ; $i <= $clones ; $i++ )
+                    $arrValue[] = $xmlBlock;
+
+            $arrValue = implode('', $arrValue);
+
+            $this->tempDocumentMainPart = str_replace($xml, $arrValue, $this->tempDocumentMainPart);
         }
 
         return $xmlBlock;
